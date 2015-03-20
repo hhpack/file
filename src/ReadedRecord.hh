31b3908c6a8192d5640cee70573de49b50c940e3
holyshared <holy.shared.design@gmail.com>
2015-03-20 09:40:54 +0000

    Add chunk trait

--- src/ReadedRecord.hh
+++ src/ReadedRecord.hh
@@ -5,40 +5,12 @@ namespace bookman\reader;
 class ReadedRecord implements ReadedResult
 {
 
+    use Chunk;
+
     public function __construct (
         private string $content
     )
     {
-    }
-
-    public function length() : int
-    {
-        return strlen($this->content);
-    }
-
-    public function value() : string
-    {
-         return $this->content;
-    }
-
-    public function substring(int $startAt, int $length) : string
-    {
-        return substr($this->content, $startAt, $length);
-    }
-
-    public function match(string $pattern) : bool
-    {
-        return preg_match($pattern, $this->content) === 1;
-    }
-
-    public function split(string $keyword) : array<string>
-    {
-        return explode($keyword, $this->content);
-    }
-
-    public function sendTo(ReadedResultReceiver $receiver) : void
-    {
-        $receiver->receiveResult($this);
     }
 
 }
