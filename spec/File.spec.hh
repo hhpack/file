d7d2a4d965443756160a3cf5009031157aa925ae
holyshared <holy.shared.design@gmail.com>
2015-03-20 09:14:43 +0000

    Add file

---
+++ spec/File.spec.hh
@@ -0,0 +1,27 @@
+<?hh //partial
+
+use bookman\File;
+
+describe('File', function() {
+    beforeEach(function() {
+        $this->path = realpath(__DIR__ . '/fixtures/text.log');
+        $this->file = File::fromString($this->path);
+    });
+    describe('#getName()', function() {
+        it('return file name', function() {
+            expect($this->file->getName())->toEqual('text.log');
+        });
+    });
+    describe('#getPath()', function() {
+        it('return file path', function() {
+            expect($this->file->getPath())->toEqual($this->path);
+        });
+    });
+    describe('#exists()', function() {
+        context('when file exists', function() {
+            it('return true', function() {
+                expect($this->file->exists())->toBeTrue();
+            });
+        });
+    });
+});
---
+++ src/File.hh
@@ -0,0 +1,66 @@
+<?hh //strict
+
+namespace bookman;
+
+use \SplFileObject;
+
+
+final class File
+{
+
+    private ?SplFileObject $file;
+    private bool $opended = false;
+
+    public function __construct(string $filePath)
+    {
+        $this->file = new SplFileObject($filePath, 'r');
+        $this->opended = true;
+    }
+
+    public function getName() : string
+    {
+        return $this->file->getFilename();
+    }
+
+    public function getPath() : string
+    {
+        return $this->file->getPathname();
+    }
+
+    public static function fromString(string $filePath) : File
+    {
+        return new self($filePath);
+    }
+
+    public function readBytes(int $length) : ReadedChunk
+    {
+        $content = $this->file->fread($length);
+        return new ReadedChunk($content);
+    }
+
+    public function readRecord() : ReadedRecord
+    {
+        $content = $this->file->fgets();
+        return new ReadedRecord($content);
+    }
+
+    public function exists() : bool
+    {
+        return file_exists($this->getPath());
+    }
+
+    public function close() : void
+    {
+        $this->file = null;
+        $this->opended = false;
+    }
+
+    public function __destruct() : void
+    {
+        if ($this->opended === false) {
+            return;
+        }
+        $this->close();
+    }
+
+}
