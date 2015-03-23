6e6d74e657fb520ff16398018aea77c003c41764
holyshared <holy.shared.design@gmail.com>
2015-03-23 06:52:14 +0000

    Add file header

--- src/File.hh
+++ src/File.hh
@@ -1,12 +1,17 @@
 <?hh //strict
 
+/**
+ * This file is part of minimalist\file package.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+
 namespace minimalist\file;
 
-//type ReadedChunk = Chunk;
-//type ReadedRecord = Chunk;
-
-//use minimalist\file\ReadedChunk;
-//use minimalist\file\ReadedRecord;
 use \SplFileObject;
 
 
--- src/FileNotFoundException.hh
+++ src/FileNotFoundException.hh
@@ -1,5 +1,14 @@
 <?hh //strict
 
+/**
+ * This file is part of minimalist\file package.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace minimalist\file;
 
 use \RuntimeException;
