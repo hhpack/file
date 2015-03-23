35d0955b3be4bf96ac80fab6ac2073e9d1c7ef5d
holyshared <holy.shared.design@gmail.com>
2015-03-23 10:22:29 +0000

    Add csv example

---
+++ src/reader/ColumnSpecification.hh
@@ -0,0 +1,54 @@
+<?hh //strict
+
+/**
+ * This file is part of minimalist\file package.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace minimalist\file\reader;
+
+
+use minimalist\file\Chunk;
+
+
+class ColumnSpecification
+{
+
+    private string $delimiter;
+    private Map<int, string> $map;
+
+    public function __construct($delimiter = ',')
+    {
+        $this->map = Map {};
+        $this->delimiter = $delimiter;
+    }
+
+    public function addColumn(int $index, string $label) : void
+    {
+        $this->map->set($index, $label);
+    }
+
+    public function parse(Chunk $record) : SeparatedRecord
+    {
+        $values = $record->split($this->delimiter);
+
+        $vector = new Vector($values);
+        $result = Map {};
+
+        foreach ($vector->getIterator() as $index => $value) {
+            if ($this->map->containsKey($index) === false) {
+                //oops!!
+            }
+            $label = $this->map->get($index);
+            $result->set($label, $value);
+        }
+//        $parseResult->toImmMap();
+
+        return new SeparatedRecord($result);
+    }
+
+}
---
+++ src/reader/SeparatedRecord.hh
@@ -0,0 +1,46 @@
+<?hh //strict
+
+/**
+ * This file is part of minimalist\file package.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace minimalist\file\reader;
+
+use \ConstCollection;
+
+class SeparatedRecord implements ConstCollection<Pair<string, string>>
+{
+
+    private ImmMap $map;
+
+    public function __construct(?KeyedTraversable<string, string> $values)
+    {
+        $this->map = new ImmMap($values);
+    }
+
+    public function get(string $label) : string
+    {
+        return $this->map->get($label);
+    }
+
+    public function count() : int
+    {
+        return $this->map->count();
+    }
+
+    public function isEmpty() : bool
+    {
+        return $this->map->isEmpty();
+    }
+
+    public function items() : Iterable<Pair<string, string>>
+    {
+        return $this->map->items();
+    }
+
+}
