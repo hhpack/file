e7362d9d6ec511e474965e4a382e5d8a4f8b45cc
holyshared <holy.shared.design@gmail.com>
2015-10-11 16:31:06 +0900

    fix spec

--- spec/FileSpec.hh
+++ spec/FileSpec.hh
@@ -1,58 +1,27 @@
 <?hh //partial
 
 use hhpack\file\File;
-use hhpack\file\FileMode;
-use hhpack\file\FileNotFoundException;
-
 
 describe(File::class, function() {
-    beforeEach(function() {
-        $this->path = realpath(__DIR__ . '/fixtures/text.log');
-        $this->file = File::fromString($this->path);
+  beforeEach(function() {
+    $this->path = realpath(__DIR__ . '/fixtures/text.log');
+    $this->file = File::fromString($this->path);
+  });
+  describe('#getName()', function() {
+    it('return file name', function() {
+      expect($this->file->getName())->toEqual('text.log');
     });
-    describe('#open()', function() {
-        beforeEach(function() {
-            $this->file->open(FileMode::READ_ONLY);
-        });
-        it('open file', function() {
-            expect($this->file->isOpened())->toBeTrue();
-        });
+  });
+  describe('#getPath()', function() {
+    it('return file path', function() {
+      expect($this->file->getPath())->toEqual($this->path);
     });
-    describe('#getName()', function() {
-        it('return file name', function() {
-            expect($this->file->getName())->toEqual('text.log');
-        });
+  });
+  describe('#exists()', function() {
+    context('when file exists', function() {
+      it('return true', function() {
+        expect($this->file->exists())->toBeTrue();
+      });
     });
-    describe('#getPath()', function() {
-        it('return file path', function() {
-            expect($this->file->getPath())->toEqual($this->path);
-        });
-    });
-    describe('#readBytes()', function() {
-        beforeEach(function() {
-            $this->file->open(FileMode::READ_ONLY);
-            $this->chunk = $this->file->readBytes(3);
-        });
-        it('return read chunk', function() {
-            expect($this->chunk->value())->toEqual('1,f');
-        });
-    });
-    describe('#readRecord()', function() {
-        beforeEach(function() {
-            $this->path = realpath(__DIR__ . '/fixtures/text.log');
-            $this->file = File::fromString($this->path);
-            $this->file->open(FileMode::READ_ONLY);
-            $this->record = $this->file->readRecord();
-        });
-        it('return read record', function() {
-            expect($this->record->value())->toEqual('1,foo');
-        });
-    });
-    describe('#exists()', function() {
-        context('when file exists', function() {
-            it('return true', function() {
-                expect($this->file->exists())->toBeTrue();
-            });
-        });
-    });
+  });
 });
