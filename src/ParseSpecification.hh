9093e41004650504bc57db6fd7b1e19a9f20ace5
holyshared <holy.shared.design@gmail.com>
2015-10-18 21:30:00 +0900

    Add src/ParseSpecification.hh

---
+++ src/ParseSpecification.hh
@@ -0,0 +1,17 @@
+<?hh //strict
+
+/**
+ * This file is part of hhpack\file package.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace hhpack\file;
+
+interface ParseSpecification<T>
+{
+    public function parse(Chunk $line) : T;
+}
