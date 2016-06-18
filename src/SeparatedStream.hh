c12077de8116461690c9bcdb386b027024de0389
holyshared <holy.shared.design@gmail.com>
2016-06-18 18:22:38 +0900

    add SeparatedStream

---
+++ src/SeparatedStream.hh
@@ -0,0 +1,47 @@
+<?hh //strict
+
+/**
+ * This file is part of hhpack\file package.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace hhpack\file;
+
+final class SeparatedStream implements Iterator<SeparatedRecord>
+{
+
+    private ParsedStream<SeparatedRecord> $stream;
+
+    public function __construct(
+        Iterator<Chunk> $stream,
+        ColumnSpecification $spec
+    )
+    {
+        $this->stream = new ParsedStream($stream, $spec);
+    }
+
+    public function current(): SeparatedRecord
+    {
+        return $this->stream->current();
+    }
+
+    public function next(): void
+    {
+        $this->stream->next();
+    }
+
+    public function rewind(): void
+    {
+        $this->stream->rewind();
+    }
+
+    public function valid(): bool
+    {
+        return $this->stream->valid();
+    }
+
+}
