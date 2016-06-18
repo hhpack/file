2aa6003e85bc2ab4c2e819619e67baaa42ef718d
holyshared <holy.shared.design@gmail.com>
2016-06-18 18:19:20 +0900

    Add ParsedStream

---
+++ src/ParsedStream.hh
@@ -0,0 +1,45 @@
+<?hh //strict
+
+/**
+ * This file is part of hhpack\file package.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace hhpack\file;
+
+final class ParsedStream<T> implements Iterator<T>
+{
+
+    public function __construct(
+        private Iterator<Chunk> $stream,
+        private ParseSpecification<T> $spec
+    )
+    {
+    }
+
+    public function current(): T
+    {
+        $chunk = $this->stream->current();
+        return $this->spec->parse($chunk);
+    }
+
+    public function next(): void
+    {
+        $this->stream->next();
+    }
+
+    public function rewind(): void
+    {
+        $this->stream->rewind();
+    }
+
+    public function valid(): bool
+    {
+        return $this->stream->valid();
+    }
+
+}
